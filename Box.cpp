d344489cba883579f57cfbecc71d5db7669473f7
Harasawa <mf23094@shibaura-it.ac.jp>
2023-12-13 13:46:17 +0900

    回転の中心を指定

--- Box.cpp
+++ Box.cpp
@@ -5,16 +5,23 @@ int Box::fig;
 int Box::figX;
 int Box::figY;
 
-Box::Box(const float cen_x, const float cen_y, const float h = 40.0f, const float w = 40.0f, const float v_x , const float v_y , const double ang )
+Box::Box(const float cen_x, const float cen_y, const float h = 40.0f, const float w = 40.0f, const float v_x , const float v_y , const float ang )
 	:height(h) , width(w) ,angle(ang) , Object(BOX)
 {
 	center.set(cen_x, cen_y);
 	velocity.set(v_x, v_y);
-	//点の追加（回転考慮せず）
-	points[0].set(center.x - width/2 , center.y - height/2);
-	points[1].set(center.x - width / 2, center.y + height / 2);
-	points[2].set(center.x + width / 2, center.y - height / 2);
-	points[3].set(center.x + width / 2, center.y + height / 2);
+	//ローカル座標の追加
+	pointsL[0].set(-width/2 , -height/2);
+	pointsL[1].set(width / 2, -height / 2);
+	pointsL[2].set(width / 2, height / 2);
+	pointsL[3].set(-width / 2, height / 2);
+	//点の追加
+	pointsW[0].set(center.x - width/2 , center.y - height/2);
+	pointsW[1].set(center.x - width / 2, center.y + height / 2);
+	pointsW[2].set(center.x + width / 2, center.y - height / 2);
+	pointsW[3].set(center.x + width / 2, center.y + height / 2);
+
+	angle_v = 0.5f;
 }
 
 void Box::loadGraph(){
@@ -27,27 +34,30 @@ void Box::updatePos(const float step) {
 	//動かない物体の場合何もしない
 	if (!active) return;
 	center = center + (velocity * step);
+	angle = angle + (angle_v * step);
 	for (int i = 0; i < 4; i++) {
-		points[i] = points[i] + (velocity * step);
+		pointsW[i] = pointsW[i] + (velocity * step);
+		//angleの分回転
+
 	}
 }
 
 void Box::Draw() const{
-	DrawBoxP(center , center , angle , fig , height , width ,figX , figY );
+	DrawBoxP(center , figX/2 , figY/2 , angle , fig , height , width ,figX , figY );
 }
 
 bool Box::isValid() const {
 	//最大値と最小値を取得
-	float max_x = points[0].x, max_y = points[0].y, min_x = points[0].x;
+	float max_x = pointsW[0].x, max_y = pointsW[0].y, min_x = pointsW[0].x;
 	for (int i = 1;  i < 4; i++) {
-		if (points[i].x > max_x) {
-			max_x = points[i].x;
+		if (pointsW[i].x > max_x) {
+			max_x = pointsW[i].x;
 		}
-		if (points[i].y > max_y) {
-			max_y = points[i].y;
+		if (pointsW[i].y > max_y) {
+			max_y = pointsW[i].y;
 		}
-		if (points[i].x < min_x) {
-			min_x = points[i].x;
+		if (pointsW[i].x < min_x) {
+			min_x = pointsW[i].x;
 		}
 	}
 	//画面外か判定
--- Box.h
+++ Box.h
@@ -6,15 +6,19 @@
 //長方形クラス
 class Box :public Object {
 private:
-	Vec2 points[4];
+	Vec2 pointsL[4];//ローカル座標での位置(左上から時計周り)
+	Vec2 pointsW[4];//ワールド座標での位置
 	float height;//高さ
 	float width;//幅
 	static int fig;
 	static int figX;
 	static int figY;
-	double angle;
+	float angle_v;
+	float angle;
+	float inertiaTensor;//慣性テンソル
+
 public:
-	Box(const float cen_x, const float cen_y, const float h  , const float w , const float v_x = 0, const float v_y = 0,const double ang = 0);
+	Box(const float cen_x, const float cen_y, const float h  , const float w , const float v_x = 0, const float v_y = 0,const float ang = 0);
 	static void loadGraph();
 	void updatePos(const float step)override;
 	void Draw()const override;
